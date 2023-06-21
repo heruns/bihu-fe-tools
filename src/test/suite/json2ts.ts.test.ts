f924f7c589a02bb2455040b1414d60e6d93f165c
heruns <hhccrr@foxmail.com>
2023-06-21 20:00:59 +0800

    添加 json2ts 单元测试

--- src/test/suite/json2ts.ts.test.ts
+++ src/test/suite/json2ts.ts.test.ts
@@ -1,6 +1,9 @@
 import * as assert from 'assert';
 import { json2ts } from '../../modules/json2ts';
 
+// `"translateProgressChapterId": 0, //全局翻译时，进度id
+// "progressChapterId": 0, //全局操作时的进度id`
+
 suite('json2ts', () => {
   test('正确格式化', () => {
     const code = `/** 测试类型 */
@@ -31,6 +34,63 @@ suite('json2ts', () => {
       /** 修改时间 */
       updatedAt: string;
     }`;
+    assert.strictEqual(json2ts(code), output);
+  });
+
+  test('已包含 |', () => {
+    const code = `/** 测试类型 */
+    export interface TestInterface {
+      translateHasQuit1: 0 | 1, //  全局翻译是否已暂停。0-否，1-是
+      translateHasQuit2: '0' | '1', //  全局翻译是否已暂停。0-否，1-是
+    }`;
+    const output = `/** 测试类型 */
+    export interface TestInterface {
+      /** 全局翻译是否已暂停。0-否，1-是 */
+      translateHasQuit1: 0 | 1,
+      /** 全局翻译是否已暂停。0-否，1-是 */
+      translateHasQuit2: '0' | '1',
+    }`;
+    assert.strictEqual(json2ts(code), output);
+  });
+
+  test('嵌套对象或数组', () => {
+    const code = `/** 测试类型 */
+    export interface TestInterface {
+      "obj": { //对象
+        key: "value",
+        "key2": 1,
+      },
+      "arr": [ //数组
+          {
+              "id": 0, //id
+          }
+      ],
+    }`;
+    const output = `/** 测试类型 */
+    export interface TestInterface {
+      /** 对象 */
+      obj: {
+        key: string;
+        key2: 1;
+      },
+      /** 数组 */
+      arr: [
+          {
+              /** id */
+              id: number;
+          }
+      ]
+    }`;
+    assert.strictEqual(json2ts(code), output);
+  });
+
+  test('非闭合数据', () => {
+    const code = `"balance":1,  //余额
+    "cardNo":""  //银行卡号`;
+    const output = `/** 余额 */
+    balance: number;
+    /** 银行卡号 */
+    cardNo: string;`;
     assert.strictEqual(json2ts(code), output);
   });
 });
