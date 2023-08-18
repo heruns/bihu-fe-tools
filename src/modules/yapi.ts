b36bdacf79d1551b7ace65ed470b51226927e351
heruns <hhccrr@foxmail.com>
2023-08-18 20:34:26 +0800

    完善 yapi 域名和 token 输入框

--- src/modules/yapi.ts
+++ src/modules/yapi.ts
@@ -2,29 +2,37 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 
+const domainRegexp = /^(https?:\/\/[^/?#]+)(?:[\/?#]|$)/i;
 export function getDomainFromUrl(url: string): string {
   url = url.trim();
   try {
     const parsedUrl = new URL(url);
-    return parsedUrl.hostname; 
+    const { protocol, hostname } = parsedUrl;
+    if (!protocol || !hostname) {
+      return '';
+    }
+    return `${protocol}//${hostname}`; 
   } catch (_) {
     // 如果 URL 解析失败,则使用正则表达式提取
-    const matched = url.match(/^https?:\/\/([^/?#]+)(?:[\/?#]|$)/i);
-    return matched ? matched[1] : url; 
+    const matched = url.match(domainRegexp);
+    return matched ? matched[1] : '';
   }
-
 }
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('yapi 模块加载成功');
 
-  const disposable = vscode.commands.registerCommand('bihu-code-snippets.config-yapi', async (uri: vscode.Uri) => {
+  // 获取域名
+  const getDomain = async (isSetting = false, showInputIfNotExist = true) => {
     const config = vscode.workspace.getConfiguration();
     const yapiDomainInSetting = config.get<string>('bihuFeTools.yapiDomain');
+    if ((yapiDomainInSetting && !isSetting) || !showInputIfNotExist) {
+      return yapiDomainInSetting;
+    }
     const yapiDomain = await vscode.window.showInputBox({
       title: '请输入 yapi 域名',
       value: yapiDomainInSetting || '',
-      placeHolder: '请输入 yapi 域名',
+      placeHolder: '请输入 yapi 域名，需包含协议，如 https://yapi.com',
       validateInput(value) {
         let error: vscode.InputBoxValidationMessage | null = null;
         if (!value.trim()) {
@@ -32,19 +40,68 @@ export function activate(context: vscode.ExtensionContext) {
             message: '请输入 yapi 域名',
             severity: 3
           };
+        } else if (!domainRegexp.test(value)) {
+          error = {
+            message: '域名格式不正确，需包含协议',
+            severity: 3
+          };
         }
         return error;
       },
     });
-    console.log('>>>> yapiDomain:', yapiDomain);
+    
+    const domain = yapiDomain ? getDomainFromUrl(yapiDomain) : yapiDomain;
+    // 修改配置
+    config.update('bihuFeTools.yapiDomain', domain, vscode.ConfigurationTarget.Workspace); 
+    return domain;
+  };
+  // 获取 token
+  const getToken = async (isSetting = false) => {
+    const config = vscode.workspace.getConfiguration();
+    const yapiTokenInSetting = config.get<string>('bihuFeTools.yapiToken');
+    if (yapiTokenInSetting && !isSetting) {
+      return yapiTokenInSetting;
+    }
+    const domain = await getDomain(false, false);
+    const yapiToken = await vscode.window.showInputBox({
+      title: '请输入 yapi 项目 token',
+      value: yapiTokenInSetting || '',
+      placeHolder: `请输入 yapi 项目 token，可在 "${domain || '{yapi 域名}'}/project/{项目id}/setting" 页面中复制`,
+      validateInput(value) {
+        let error: vscode.InputBoxValidationMessage | null = null;
+        if (!value.trim()) {
+          error = {
+            message: '请输入 yapi 项目 token',
+            severity: 3
+          };
+        }
+        return error;
+      },
+    });
+    console.log('>>>> yapiToken:', yapiToken);
     
     // 修改配置
-    config.update('bihuFeTools.yapiDomain', yapiDomain, vscode.ConfigurationTarget.Workspace); 
-    if (typeof yapiDomain === 'string') {
-      return yapiDomain.trim();
-    } else {
-      return yapiDomain;
-    }
+    config.update('bihuFeTools.yapiToken', yapiToken, vscode.ConfigurationTarget.Workspace); 
+    return yapiToken;
+  };
+  interface DomainAndToken {
+    domain: string;
+    token: string;
+  }
+  // 获取域名和 token
+  const getDomainAndToken = async() => {
+    const domain = await getDomain();
+    const token = await getToken();
+    const res: DomainAndToken = {
+      domain: domain || '',
+      token: token || ''
+    };
+    return res;
+  };
+
+  const disposable = vscode.commands.registerCommand('bihu-code-snippets.config-yapi', async (uri: vscode.Uri) => {
+    await getDomain(true);
+    await getToken(true);
   });
 
   context.subscriptions.push(disposable);
--- src/test/suite/yapi.ts.test.ts
+++ src/test/suite/yapi.ts.test.ts
@@ -4,24 +4,24 @@ import { getDomainFromUrl } from '../../modules/yapi';
 suite('getDomainFromUrl', () => {
   test('should get domain from valid http url', () => {
     const url = 'http://www.example.com/path';
-    const expectedDomain = 'www.example.com';
+    const expectedDomain = 'http://www.example.com';
     assert.equal(getDomainFromUrl(url), expectedDomain);
   });
 
   test('should get domain from valid https url', () => {
     const url = 'https://www.example.com/path/abc?a=1';
-    const expectedDomain = 'www.example.com';
+    const expectedDomain = 'https://www.example.com';
     assert.equal(getDomainFromUrl(url), expectedDomain);
   });
 
   test('should get domain from url without protocol', () => {
     const url = 'www.example.com';
-    const expectedDomain = 'www.example.com';
+    const expectedDomain = '';
     assert.equal(getDomainFromUrl(url), expectedDomain); 
   });
 
   test('should return input for invalid url', () => {
     const invalidUrl = 'foo';
-    assert.equal(getDomainFromUrl(invalidUrl), invalidUrl);
+    assert.equal(getDomainFromUrl(invalidUrl), '');
   });
 });
